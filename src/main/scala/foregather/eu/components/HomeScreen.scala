7b1346c9d736590a3c03ed729f964653a154bf91
Ludovic Guegan <ludovic.guegan@gmail.com>
2017-10-04 00:49:11 +0200

    layout views

--- src/main/scala/foregather/eu/components/HomeScreen.scala
+++ src/main/scala/foregather/eu/components/HomeScreen.scala
@@ -15,20 +15,22 @@ class HomeScreen extends NavigationScreenComponentNoPS {
   import HomeScreen._
   def render() = {
     View(style = styles.container)(
-      getBlock(() => navigation.navigate[QuizScreen]( new Params { override val qcm: QCM = DataSet.next }),
-               "Quiz")
+        View(style = styles.top)(
+            View(style = styles.progress)(),
+            View(style = styles.rightBlock)(
+                View(style = styles.character)(),
+                View(style = styles.score)()
+            )
+        ),
+        View(style = styles.bottom)(
+            View(style = styles.history)(),
+            TouchableHighlight(
+              style = styles.run,
+              onPress = () => navigation.navigate[QuizScreen]( new Params { override val qcm: QCM = DataSet.next })
+              )("Quiz")
+        )
     )
   }
-
-  @inline
-  def getBlock(onPress: () => _, title: String) =
-    TouchableOpacity(
-      style = styles.block,
-      onPress = onPress,
-      activeOpacity = 0.8
-    )(
-      Text(style = styles.blockText)(title)
-    )
 }
 
 object HomeScreen {
@@ -38,30 +40,32 @@ object HomeScreen {
     import dsl._
 
     val container = style(
+      flex := 1
+    )
+    val top = style(
       flex := 1,
-      padding := 20,
-      flexDirection.row,
-      flexWrap.wrap
+      flexDirection := "row"
     )
-
-    val block = style(
-      width := "42%",
-      height := 120,
-      backgroundColor := "white",
-      borderColor := "#eee",
-      borderRadius := 2,
-      margin := 10,
-      paddingHorizontal := 3,
-      shadowColor := "grey",
-      shadowOpacity := 0.5,
-      shadowRadius := 2,
-      shadowOffset := js.Dynamic.literal(height = 1, width = 0),
-      elevation := 3,
-      borderWidth := 1,
-      justifyContent.center,
-      alignItems.center
+    val bottom = style(
+      flex := 1
     )
-
-    val blockText = style(fontWeight := "500", fontSize := 14)
+    val progress = style(
+      flex := 1
+    )
+    val history = style(
+      flex := 1
+    )
+    val rightBlock = style(
+      flex := 1
+    )
+    val character = style(
+      flex := 1
+    )
+    val score = style(
+      flex := 1
+    )
+    val run = style(
+      flex := 1
+    )
   }
 }
--- src/main/scala/foregather/eu/components/QuizScreen.scala
+++ src/main/scala/foregather/eu/components/QuizScreen.scala
@@ -15,28 +15,25 @@ class QuizScreen
     extends NavigationScreenComponentP[QuizScreen.Params] {
   import QuizScreen._
   def render() = {
-    View(style = GlobalStyles.wholeContainer)(
-      TextC("Question: " + params.get.qcm.question),
-      View(style = styles.container)(
-        getBlock(() => navigation.navigate[HomeScreen],
-          params.get.qcm.answers._1),
-        getBlock(() => navigation.navigate[HomeScreen],
-          params.get.qcm.answers._2),
-        getBlock(() => navigation.navigate[HomeScreen],
-          params.get.qcm.answers._3),
-        getBlock(() => navigation.navigate[HomeScreen],
-          params.get.qcm.answers._4)
-        ))
-  }
-  @inline
-  def getBlock(onPress: () => _, title: String) =
-    TouchableOpacity(
-      style = styles.block,
-      onPress = onPress,
-      activeOpacity = 0.8
-    )(
-      Text(style = styles.blockText)(title)
+    View(style = styles.container)(
+      View(style = styles.top)(TextC("Progress")),
+      View(style = styles.middle)(TextC(params.get.qcm.question)),
+      View(style = styles.bottom)(
+        View(style = styles.columnAnswers)(
+          TouchableHighlight( style = styles.answer,
+            onPress = () => navigation.navigate[HomeScreen])(params.get.qcm.answers._1),
+          TouchableHighlight( style = styles.answer,
+            onPress = () => navigation.navigate[HomeScreen])(params.get.qcm.answers._3)
+        ),
+        View(style = styles.columnAnswers)(
+          TouchableHighlight( style = styles.answer,
+            onPress = () => navigation.navigate[HomeScreen])(params.get.qcm.answers._2),
+          TouchableHighlight( style = styles.answer,
+            onPress = () => navigation.navigate[HomeScreen])(params.get.qcm.answers._4)
+        )
+      )
     )
+  }
 }
 
 object QuizScreen {
@@ -49,31 +46,27 @@ object QuizScreen {
     import dsl._
 
     val container = style(
+      flex := 1
+    )
+    val top = style(
+      flex := 1
+    )
+    val middle = style(
+      flex := 1
+    )
+    val bottom = style(
+      flex := 2,
+      flexDirection := "row"
+    )
+    val columnAnswers = style(
       flex := 1,
-      padding := 20,
-      flexDirection.row,
-      flexWrap.wrap
+      justifyContent := "space-around",
+      alignItems := "center"
     )
-
-    val block = style(
-      width := "42%",
-      height := 120,
-      backgroundColor := "white",
-      borderColor := "#eee",
-      borderRadius := 2,
-      margin := 10,
-      paddingHorizontal := 3,
-      shadowColor := "grey",
-      shadowOpacity := 0.5,
-      shadowRadius := 2,
-      shadowOffset := js.Dynamic.literal(height = 1, width = 0),
-      elevation := 3,
-      borderWidth := 1,
-      justifyContent.center,
-      alignItems.center
+    val answer = style(
+      flex := 1,
+      backgroundColor := "skyblue"
     )
-
-    val blockText = style(fontWeight := "500", fontSize := 14)
   }
 }
 
