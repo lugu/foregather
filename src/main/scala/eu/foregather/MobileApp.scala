7a9682df6ff59212c91544e4e6c1c22acaed9409
Ludovic Guegan <ludovic.guegan@gmail.com>
2017-10-05 22:18:12 +0200

    remove main override warning

--- src/main/scala/eu/foregather/MobileApp.scala
+++ src/main/scala/eu/foregather/MobileApp.scala
@@ -3,8 +3,5 @@ package eu.foregather
 import sri.universal.apis.AppRegistry
 
 object MobileApp extends App {
-
-  override def main(args:Array[String]) = {
-    AppRegistry.registerComponent("foregather", () => components.root)
-  }
+  AppRegistry.registerComponent("foregather", () => components.root)
 }
