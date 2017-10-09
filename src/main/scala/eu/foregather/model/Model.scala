142aa444f3e8e1dea6224b730e92059bd6ffcb2c
Ludovic Guegan <ludovic.guegan@gmail.com>
2017-10-09 22:04:22 +0200

    Use days since epoch for history

--- src/main/scala/eu/foregather/model/Model.scala
+++ src/main/scala/eu/foregather/model/Model.scala
@@ -16,19 +16,25 @@ case class Quiz(
 }
 
 case object Today {
-  // import java.time.LocalDate
-  // import java.time.temporal.ChronoUnit
-  // val epoch = LocalDate.ofEpochDay(0);
-  def is = 0 // ChronoUnit.DAYS.between(epoch, LocalDate.now())
+  import scala.scalajs.js.Date
+  val msPerDay = 1000 * 60 * 60 * 24
+  val epoch = new Date(1970, 1, 1, 0, 0, 0, 0)
+  def now = new Date()
+  def daysSinceEpoch = ((now.getTime() - epoch.getTime()) / msPerDay).floor.toInt
+  def is: Int = daysSinceEpoch
 }
 
 case class History(activities: List[Activity]) {
-  def updateWith(d: Int, a: Int) = History(Activity(d, a) :: activities)
-  def string: String = activities.mkString(",")
+  def updateWith(d: Int, q: Int) = {
+    activities.find((a: Activity) => a.day == d) match {
+      case Some(a) => History(activities.map((a: Activity) =>
+          if (a.day == d) Activity(d, a.quizNb + q) else a))
+      case None => History(Activity(d, q) :: activities)
+    }
+  }
 }
 
 case class Activity(day: Int, quizNb: Int) {
-  def string: String = List(day, quizNb).mkString("-")
 }
 
 case class Profile(name: String, score: Int, history: History)
@@ -43,19 +49,28 @@ trait ProfileWatcher {
 
 object Circuit {
   var watcher: List[ProfileWatcher] = List()
-  ProfileStorage.retrive((p: Profile) => setState(p))
+
   var profile = Profile("Unknown", 0, History(List()))
   def initialState = profile
-  val actionHandler = (action: Action, model: Profile) => action match {
-    case (CorrectAnswer) => setState(model.copy(score = model.score + 100, history = model.history.updateWith(Today.is, 1)))
-    case (WrongAnswer) => setState(model.copy(score = model.score, history = model.history.updateWith(Today.is, 1)))
+
+  val actionHandler = (action: Action, model: Profile) => {
+    val history = model.history.updateWith(Today.is, 1)
+    val score = action match {
+      case (CorrectAnswer) => model.score + 100
+      case (WrongAnswer) => model.score
+    }
+    setState(model.copy(score = score + 100, history = history))
   }
+
   def setState(p: Profile) = {
     profile = p
     watcher.foreach { w => w.onProfileChange(p) }
     ProfileStorage.store(p)
   }
+
   def registerWatcher(w: ProfileWatcher) = { watcher = w :: watcher }
   def unregisterWatcher(w: ProfileWatcher) = { watcher = watcher.filterNot(a => a == w) }
+
+  ProfileStorage.retrive((p: Profile) => setState(p))
 }
 
